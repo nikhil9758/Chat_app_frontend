fb982cd86df3766f76c3ca6fada2517e90ccc565
Nikhil Gupta <nikhil_gu@hcl.com>
2024-02-04 11:38:43 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 
-function App() {
+const App = () => {
+  const [socket, setSocket] = useState(null);
+  const [message,setMessage]= useState('')
+  const[data,setData]=useState({})
+  useEffect(() => {
+    const newSocket = io('http://localhost:5000');
+    setSocket(newSocket)
+    newSocket.on('connect',()=>{
+          console.log("conected.....")
+    })
+
+    return () => newSocket.disconnect();
+  }, []);
+
+    const handleOnSubmit=()=>{
+       socket.emit('sendMessage',message)
+       socket.on('message',(item)=>{
+            console.log("here is the item.....",item)
+            setData(item)
+       })
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        here
+        <input type='text' onChange={(e)=>setMessage(e.target.value)}/>
+        <button type='submit' onClick={handleOnSubmit}>send</button>
+        {data?.text}
+        {data?.user}
     </div>
   );
-}
+};
 
 export default App;
